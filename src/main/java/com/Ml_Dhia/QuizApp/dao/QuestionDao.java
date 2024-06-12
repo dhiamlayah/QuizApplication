3783586384c3deb70f9dcb8634053d45a36a216e
dhiamlayah <mlayahdhia8@gmail.com>
2024-06-12 11:10:14 +0100

    adding new route for getting data by category

--- src/main/java/com/Ml_Dhia/QuizApp/dao/QuestionDao.java
+++ src/main/java/com/Ml_Dhia/QuizApp/dao/QuestionDao.java
@@ -4,8 +4,11 @@ import com.Ml_Dhia.QuizApp.model.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface QuestionDao extends JpaRepository<Question,Integer> {
-
+    /* we can add this methode and jpa will implement it but if we want to castemize it we should use hql or jpql */
+    List<Question> findByCategory(String category);
 
 }
