8b433531db992c16fd854285eb702d367ccb6e90
dhiamlayah <mlayahdhia8@gmail.com>
2024-06-18 11:09:52 +0100

    getting quiz question for user

--- src/main/java/com/Ml_Dhia/QuizApp/controller/QuizController.java
+++ src/main/java/com/Ml_Dhia/QuizApp/controller/QuizController.java
@@ -1,13 +1,14 @@
 package com.Ml_Dhia.QuizApp.controller;
 
+import com.Ml_Dhia.QuizApp.model.Question;
+import com.Ml_Dhia.QuizApp.model.QuestionWrapper;
 import com.Ml_Dhia.QuizApp.service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("quiz")
@@ -19,6 +20,11 @@ public class QuizController {
     @PostMapping("create")
     public ResponseEntity<String> createQuiz(@RequestParam String category , @RequestParam int numOfQuestions , @RequestParam  String Title ){
         return  quizService.createQuiz(category, numOfQuestions, Title) ;
+    }
+
+    @GetMapping("get/{id}")
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable int id){
+        return  quizService.getQuizQuestions( id );
     }
 
 }
--- src/main/java/com/Ml_Dhia/QuizApp/service/QuizService.java
+++ src/main/java/com/Ml_Dhia/QuizApp/service/QuizService.java
@@ -3,13 +3,17 @@ package com.Ml_Dhia.QuizApp.service;
 import com.Ml_Dhia.QuizApp.dao.QuestionDao;
 import com.Ml_Dhia.QuizApp.dao.QuizDao;
 import com.Ml_Dhia.QuizApp.model.Question;
+import com.Ml_Dhia.QuizApp.model.QuestionWrapper;
 import com.Ml_Dhia.QuizApp.model.Quiz;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class QuizService {
@@ -26,5 +30,17 @@ public class QuizService {
         quizDao.save(quiz);
         return  new ResponseEntity<String>("success", HttpStatus.OK);
 
+    }
+
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions (int id ){
+        Optional<Quiz> getQuizFromDB =    quizDao.findById(id);
+        List<Question> getQuestionsFromQuiz = getQuizFromDB.get().getQuestions();
+        List<QuestionWrapper> questionForUser = new ArrayList<>();
+        for (Question q :getQuestionsFromQuiz  ){
+            QuestionWrapper questionWrapper = new QuestionWrapper(q.getId() , q.getQuestion() , q.getOption1(), q.getOption2(), q.getOption3(), q.getOption4());
+            questionForUser.add(questionWrapper);
+        }
+
+        return new  ResponseEntity<>(questionForUser , HttpStatus.OK);
     }
 }
